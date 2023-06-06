de7c5251128705d4f1bf5510ef13d7fccfad2b20
Erik Jr Hupka <erik.hupka1@gmail.com>
2023-06-06 23:31:56 +0200

    Some updates

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,26 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/app/layout.tsx": [
+      "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/node_modules/next/font/google/target.css?{\"arguments\":[{\"subsets\":[\"latin\"]}],\"import\":\"Inter\",\"path\":\"app/layout.tsx\",\"variableName\":\"inter\"}",
+      "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/app/page": [
+      "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/app/globals.css",
+      "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/node_modules/next/font/google/target.css?{\"arguments\":[{\"subsets\":[\"latin\"]}],\"import\":\"Inter\",\"path\":\"app/layout.tsx\",\"variableName\":\"inter\"}"
+    ],
+    "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/app/about/page": [
+      "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/app/globals.css",
+      "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/node_modules/next/font/google/target.css?{\"arguments\":[{\"subsets\":[\"latin\"]}],\"import\":\"Inter\",\"path\":\"app/layout.tsx\",\"variableName\":\"inter\"}"
+    ],
+    "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/app/settings/page": [
+      "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/app/globals.css",
+      "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/node_modules/next/font/google/target.css?{\"arguments\":[{\"subsets\":[\"latin\"]}],\"import\":\"Inter\",\"path\":\"app/layout.tsx\",\"variableName\":\"inter\"}"
+    ],
+    "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/app/feedback/page": [
+      "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/app/globals.css",
+      "/Users/erik_jr_hupka/Projects/DVLPR_To-Do_App/node_modules/next/font/google/target.css?{\"arguments\":[{\"subsets\":[\"latin\"]}],\"import\":\"Inter\",\"path\":\"app/layout.tsx\",\"variableName\":\"inter\"}"
+    ]
+  }
+}
